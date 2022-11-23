f768c988e916d165a731e6c71b79b5d7dbcebeb5
js <jasdepw@gmail.com>
2022-11-23 17:17:15 +0900

    Feat: get article data from jsonl files

---
+++ tools/get_article_data.py
@@ -0,0 +1,44 @@
+import jsonlines
+import pandas as pd
+import re
+from dotenv import load_dotenv
+from os import getenv
+from tqdm import tqdm
+
+
+# load env
+load_dotenv()
+# jsonl article file path
+TRAIN_ARTICLE_PATH = getenv('TRAIN_ARTICLE_PATH')
+TEST_ARTICLE_PATH = getenv('TEST_ARTICLE_PATH')
+VAL_ARTICLE_PATH = getenv('VAL_ARTICLE_PATH')
+# article folder path
+ARTICLE_FOLDER_PATH = getenv('ARTICLE_FOLDER_PATH')
+
+
+def remove_emoji(text):
+    return re.sub(r'[^A-Za-z0-9.,?!@#$^&*(){};:%`\n\"\' ]', '', text)
+
+
+def get_article_data(file_path):
+    categories = []
+    texts = []
+    regex = re.compile('{}(.*){}'.format(re.escape('_'), re.escape('.')))
+    file_name = regex.findall(file_path)[0]
+
+    with jsonlines.open(file_path) as f:
+        for line in tqdm(f):
+            categories.append(re.sub(r'[^A-Za-z-]', '', line['id'])[:-1])
+            texts.append(remove_emoji(line['text']))
+
+    article_df = pd.DataFrame({'category': categories, 'text': texts})
+    article_df.to_csv(ARTICLE_FOLDER_PATH+'/{}/{}.csv'.format(file_name, file_name), index=False)
+    
+    
+def main():
+    file_list = [TRAIN_ARTICLE_PATH, TEST_ARTICLE_PATH, VAL_ARTICLE_PATH]
+    [get_article_data(file_path) for file_path in file_list]
+
+
+if __name__ == "__main__":
+    main()
